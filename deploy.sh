2ac81b3107195c5aef514c67c717dc1a1df69477
Max Mathieu <max@heatmap.me>
2017-11-17 13:33:28 -0500

    Fix line delimiters

--- deploy.sh
+++ deploy.sh
@@ -32,11 +32,12 @@ fi
 
 # Check version in readme.txt is the same as plugin file after translating both to unix line breaks to work around grep's failure to identify mac line breaks
 NEWVERSION1=`grep "^Stable tag:" $GITPATH/readme.txt | awk -F' ' '{print $NF}'`
-echo "readme.txt version: $NEWVERSION1"
+echo "readme.txt version: [$NEWVERSION1]"
 NEWVERSION2=`grep "^Version:" $GITPATH/$MAINFILE | awk -F' ' '{print $NF}'`
-echo "$MAINFILE version: $NEWVERSION2"
+echo "$MAINFILE version: [$NEWVERSION2]"
 
 if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Version in readme.txt & $MAINFILE don't match. Exiting...."; exit 1; fi
+exit 1;
 
 echo "Versions match in readme.txt and $MAINFILE. Let's proceed..."
 
